da6de7bd4a9173f40cdb8e2b14a59953ff10b96f
adhicini <adhicini@berkeley.edu>
2024-12-20 17:29:35 -1000

    delete duplicate line in server.js

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log(`Incoming Request: ${req.method} ${req.url}`);
-  next();
-});
-
-
 app.use((req, res) => {
   res.status(404).json({ error: `Route ${req.url} not found` });
 });
